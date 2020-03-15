5d11f1b97f64152c9ce583e99f30c456da8ed026
yxuko <yxuko@outlook.com>
2020-03-15 11:32:08 +0100

    Add Vim

--- vim/install.sh
+++ vim/install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-curl -fLo "$DOTFILES/vim/autoload/plug.vim" --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall
-vim +PlugUpdate
+vim +'PlugInstall --sync' +qa
+vim +'PlugUpdate' +qa
 
-if which neovim >/dev/null 2>&1; then
+if command -v nvim >/dev/null 2>&1; then
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 	mkdir -p ~/.config/nvim/
 	ln -sf "$DOTFILES/vim/vimrc.symlink" ~/.config/nvim/init.vim
-	nvim +PlugInstall
-	nvim +PlugUpdate
+	nvim +'PlugInstall --sync' +qa
+	nvim +'PlugUpdate' +qa
 fi
