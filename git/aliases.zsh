215fa0c3988ccfbdb464ad5c7bf80e14f0e3890b
yxuko <yxuko@outlook.com>
2020-03-15 11:31:37 +0100

    Add Git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,24 +1,31 @@
 #!/bin/sh
+if command -v hub >/dev/null 2>&1; then
+	alias git='hub'
+fi
+
 alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias glg="git log --graph --decorate --oneline --abbrev-commit"
+alias glga="glg --all"
+alias gp='git push origin HEAD'
+alias gpa='git push origin --all'
 alias gd='git diff'
-alias gc='git clone'
-alias gca='git commit -a'
+alias gc='git commit -s'
+alias gca='git commit -s -a'
 alias gco='git checkout'
-alias gb='git branch'
+alias gb='git branch -v'
 alias ga='git add'
 alias gaa='git add -A'
-alias gcm='git commit -S -m'
+alias gcm='git commit -s -m'
+alias gcam='git commit -s -a -m'
 alias gs='git status -sb'
-alias gcb='git-copy-branch-name'
-alias gst='git status'
-alias grh='git reset HEAD'
-alias gfu='git fetch upstream'
-alias gusub='git submodule update --recursive --remote'
-alias gp='git push'
-alias gpo='git-promote'
+alias gpr='gp && git pr'
+alias glnext='git log --oneline $(git describe --tags --abbrev=0 @^)..@'
+
+if command -v svu >/dev/null 2>&1; then
+	alias gtpatch='echo `svu p`; git tag `svu p`'
+	alias gtminor='echo `svu m`; git tag `svu m`'
+fi
 
 gi() {
-  curl -s "https://www.gitignore.io/api/$*";
+	curl -s "https://www.gitignore.io/api/$*"
 }
