2e65bd20932097e909a8c6cb0c239058e92cbef4
yxuko <yxuko@outlook.com>
2020-12-21 07:12:39 +0100

    Cleanup

--- fisher/install.fish
+++ fisher/install.fish
@@ -1,9 +1,5 @@
 #!/usr/bin/env fish
 
-if !command -qs code
-	exit 0
-end
-
 cat $DOTFILES/fisher/plugins | while read module
 	fisher install "$module"
 		and echo "vscode: $module"
--- ssh/install.fish
+++ ssh/install.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-test -L ~/.ssh/config ||
+if test -e ~/.ssh/config
 	mv ~/.ssh/config ~/.ssh/config.local
 	and ln -s "$DOTFILES"/ssh/config ~/.ssh/config
 end
