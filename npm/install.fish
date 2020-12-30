0abf6a532d81ef3a37f81d1def3aee2810704a26
Yacine Kanzari <yxuko@outlook.com>
2020-12-30 10:01:04 +0100

    Add more Npm abbreviations

--- npm/install.fish
+++ npm/install.fish
@@ -1,7 +1,13 @@
 #!/usr/bin/env fish
 
+abbr -a npb 'npm build'
+abbr -a ncl 'npm clean'
+abbr -a npi 'npm install'
 abbr -a npis 'npm install --save'
 abbr -a npisd 'npm install --save-dev'
 abbr -a npig 'npm install -g'
 abbr -a npit 'npm init'
-abbr -a npi 'npm install'
+abbr -a nps 'npm serve'
+abbr -a npst 'npm start'
+abbr -a npt 'npm test'
+abbr -a npv 'npm --version'
