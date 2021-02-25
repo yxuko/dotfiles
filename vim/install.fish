1beef33dd9668a2a1a3d56b05ce3dfa4d75996f9
Yacine Kanzari <1786317+yxuko@users.noreply.github.com>
2021-02-25 15:02:37 +0100

    Fix vim and nvim

--- vim/install.fish
+++ vim/install.fish
@@ -1,13 +1,14 @@
 #!/usr/bin/env fish
 
-if ! command -qs nvim
+if command -qs vim
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	vim +'PlugUpdate | PlugInstall --sync' +qa
-else
+end
+if command -qs nvim
 	abbr -a vim 'nvim'
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 	mkdir -p ~/.config/nvim/
 		and ln -sf "$DOTFILES/vim/vimrc.symlink" ~/.config/nvim/init.vim
-		and vim +'PlugUpdate | PlugInstall --sync' +qa
+		and nvim +'PlugUpdate | PlugInstall --sync' +qa
 end
